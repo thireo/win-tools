e3ff3e6dd164250b64ae27ede8d8831f05422e4e
ATHIR-MCBK-UBUNTU <at@danishcare.dk>
2019-07-10 20:28:20 +0200

    Sorted list with vim. Added -r to every choco line to ensure minimal output.

--- chocostart.bat
+++ chocostart.bat
@@ -1,43 +1,42 @@
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object
 System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
-choco install steam -y
-choco install foxitreader -y
-choco install git -y
-choco install gitkraken -y
-choco install paint.net -y
-choco install filezilla -y
-choco install notepadplusplus -y
-choco install 7zip -y
-choco install vlc -y
-choco install googlechrome -y
-choco install dropbox -y
-choco install vscode -y
-choco install androidstudio -y
-choco install beyondcompare -y
-choco install miktex -y
-choco install texstudio -y
-choco install kicad -y
-choco install openvpn -y
-choco install audacity -y
-choco install hxd -y
-choco install putty -y
-choco install spotify -y
-choco install jdk8 -y
-choco install etcher -y
-choco install hyper -y
-choco install putty -y
-choco install python3 -y
-choco install discord -y
-choco install plexmediaserver -y
-choco install origin -y
-choco install vnc-viewer -y
-choco install obs-studio -y
-choco install qbittorrent -y
-choco install freecad -y
-choco install advanced-ip-scanner -y
-choco install prusaslicer -y
-choco install blender -y
-choco install vim -y
-choco install golang -y
-choco install atom -y
+choco install steam -y -r
+choco install foxitreader -y -r
+choco install git -y -r
+choco install gitkraken -y -r
+choco install paint.net -y -r
+choco install filezilla -y -r
+choco install notepadplusplus -y -r
+choco install 7zip -y -r
+choco install vlc -y -r
+choco install googlechrome -y -r
+choco install dropbox -y -r
+choco install vscode -y -r
+choco install androidstudio -y -r
+choco install beyondcompare -y -r
+choco install miktex -y -r
+choco install texstudio -y -r
+choco install kicad -y -r
+choco install openvpn -y -r
+choco install audacity -y -r
+choco install hxd -y -r
+choco install putty -y -r
+choco install spotify -y -r
+choco install jdk8 -y -r
+choco install etcher -y -r
+choco install hyper -y -r
+choco install putty -y -r
+choco install python3 -y -r
+choco install discord -y -r
+choco install plexmediaserver -y -r
+choco install origin -y -r
+choco install vnc-viewer -y -r
+choco install obs-studio -y -r
+choco install qbittorrent -y -r
+choco install freecad -y -r
+choco install advanced-ip-scanner -y -r
+choco install prusaslicer -y -r
+choco install blender -y -r
+choco install vim -y -r
+choco install golang -y -r
+choco install atom -y -r
