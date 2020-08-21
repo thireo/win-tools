8407e0bed589d3d53d8af564068c487f3e0de364
Andreas <dk.andreas@gmail.com>
2020-08-21 18:04:18 +0200

    Changed foxitreader to adobereader
    Removed plexmediaserver
    
    Removed windows terminal
    
    added avrdude, insomnia, gnuarmembedded

--- chocostart.bat
+++ chocostart.bat
@@ -1,14 +1,13 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object
 System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco install steam -y -r
-choco install foxitreader -y -r
+choco install adobereader -y -r
 choco install git -y -r
 choco install paint.net -y -r
 choco install filezilla -y -r
 choco install notepadplusplus -y -r
 choco install 7zip -y -r
-choco install vlc -y -r
-choco install googlechrome -y -r
+choco install vlc -y -
 choco install vscode -y -r
 choco install androidstudio -y -r
 choco install beyondcompare -y -r
@@ -25,7 +24,6 @@ choco install etcher -y -r
 choco install putty -y -r
 choco install python3 -y -r
 choco install discord -y -r
-choco install plexmediaserver -y -r
 choco install origin -y -r
 choco install vnc-viewer -y -r
 choco install obs-studio -y -r
@@ -35,5 +33,9 @@ choco install prusaslicer -y -r
 choco install blender -y -r
 choco install vim -y -r
 choco install golang -y -r
-choco install microsoft-windows-terminal -y -r
+rem choco install microsoft-windows-terminal -y -r
 choco install sqLitebrowser -y -r
+choco install insomnia-rest-api-client -y -r
+choco install avrdude -y -r
+choco install zephyr-gnuarmemb -y -r
+choco install gcc-arm-embedded -y -r
