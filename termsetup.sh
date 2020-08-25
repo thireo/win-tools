fcefc83d3a775a610b2697306e50ee8ce56b0c67
athireo <andreas@thirsbro.dk>
2020-08-25 21:12:29 +0200

    Updated termsetup.sh to fix issue with USERNAME

--- termsetup.sh
+++ termsetup.sh
@@ -1,2 +1,21 @@
-cat .bashrc >> ~/.bashrc
-echo Appended .bashrc to USERPROFILE
+read -p "You didn't just ran me without admin privileges, did you?"
+
+echo "Okay fine. I'll go ahead then."
+#cat .bashrc >> ~/.bashrc
+#echo Appended .bashrc to USERPROFILE
+
+
+
+rm -rf wt.reg
+echo 'Windows Registry Editor Version 5.00' >> wt.reg
+echo '' >> wt.reg
+echo '[HKEY_CLASSES_ROOT\Directory\Background\shell\wt]' >> wt.reg
+echo '@="Terminal here"' >> wt.reg
+echo '"Icon"="C:\\Program Files\\Git\\git-bash.exe"' >> wt.reg
+echo '' >> wt.reg
+echo '[HKEY_CLASSES_ROOT\Directory\Background\shell\wt\command]' >> wt.reg
+printf '@=\"\\\"C:\\\\Users\\\\%s\\\\AppData\\\\Local\\\\Microsoft\\\\WindowsApps\\\\wt.exe\\\" \\\"-d\\\" \\\"%%v.\\\"\"' $USERNAME >> wt.reg
+
+echo "Importing new wt.reg file"
+regedit -S wt.reg
+echo "Damn I'm good!"
