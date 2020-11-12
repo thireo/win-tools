97bcaff2ac9cab5e7459eb34728261fb937c140e
Andreas Thirsbro <at@danishcare.dk>
2020-11-12 10:00:29 +0100

    Added context menu shortcut to elevated Windows terminal

--- termsetup.sh
+++ termsetup.sh
@@ -6,16 +6,17 @@ echo "Okay fine. I'll go ahead then."
 
 
 
-rm -rf wt.reg
-echo 'Windows Registry Editor Version 5.00' >> wt.reg
-echo '' >> wt.reg
-echo '[HKEY_CLASSES_ROOT\Directory\Background\shell\wt]' >> wt.reg
-echo '@="Terminal here"' >> wt.reg
-echo '"Icon"="C:\\Program Files\\Git\\git-bash.exe"' >> wt.reg
-echo '' >> wt.reg
-echo '[HKEY_CLASSES_ROOT\Directory\Background\shell\wt\command]' >> wt.reg
-printf '@=\"\\\"C:\\\\Users\\\\%s\\\\AppData\\\\Local\\\\Microsoft\\\\WindowsApps\\\\wt.exe\\\" \\\"-d\\\" \\\"%%v.\\\"\"' $USERNAME >> wt.reg
+#rm -rf wt.reg
+#echo 'Windows Registry Editor Version 5.00' >> wt.reg
+#echo '' >> wt.reg
+#echo '[HKEY_CLASSES_ROOT\Directory\Background\shell\wt]' >> wt.reg
+#echo '@="Terminal here"' >> wt.reg
+#echo '"Icon"="C:\\Program Files\\Git\\git-bash.exe"' >> wt.reg
+#echo '' >> wt.reg
+#echo '[HKEY_CLASSES_ROOT\Directory\Background\shell\wt\command]' >> wt.reg
+#printf '@=\"\\\"C:\\\\Users\\\\%s\\\\AppData\\\\Local\\\\Microsoft\\\\WindowsApps\\\\wt.exe\\\" \\\"-d\\\" \\\"%%v.\\\"\"' $USERNAME >> wt.reg
 
 echo "Importing new wt.reg file"
 regedit -S wt.reg
+regedit -S wte.reg
 echo "Damn I'm good!"
